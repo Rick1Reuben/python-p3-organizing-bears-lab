5e1f3f57d9b4d6c9b187950880e3b399586a93ef
Reuben Kamau <rickr6129@gmail.com>
2024-06-06 21:20:48 +0300

    Lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 4, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'brown', 'friendly', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'aggressive', 0),
+('Melissa', 7, 'F', 'black', 'friendly', 1),
+('Grinch', 5, 'M', 'black', 'aggressive', 1),
+('Wendy', 8, 'F', 'brown', 'calm', 1),
+(NULL, 3, 'F', 'brown', 'friendly', 1);
